ac8395b2223ad4065a8e56d107c077df73bf7edf
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2021-04-08 15:37:47 +0200

    Add Javadoc

--- src/main/java/com/m2i/devops/Application.java
+++ src/main/java/com/m2i/devops/Application.java
@@ -1,7 +1,19 @@
 package com.m2i.devops;
 
+/**
+ * Main class of Application.
+ *
+ * @author micky
+ * @version 1.0
+ *
+ */
 public class Application {
 
+  /**
+   * Entry-point function of Application.
+   *
+   * @param args command line arguments array.
+   */
   public static void main(String[] args) {
     System.out.println("Hello W !");
   }
--- src/main/java/com/m2i/devops/Calculator.java
+++ src/main/java/com/m2i/devops/Calculator.java
@@ -1,23 +1,64 @@
 package com.m2i.devops;
 
+/**
+ * Calculator engine class.
+ * Provide many function...
+ *
+ * @author Mickael Gaillard
+ * @version 1.0
+ */
 public class Calculator {
 
+  /**
+   * Function Add two value.
+   *
+   * @param a first value.
+   * @param b second value.
+   * @return result of addition.
+   */
   public int add(int a, int b) {
     return a + b;
   }
 
+  /**
+   * Function Subtract two value.
+   *
+   * @param a first value.
+   * @param b second value.
+   * @return result of Subtraction.
+   */
   public int sub(int a, int b) {
     return a - b;
   }
 
+  /**
+   * Function Multiply two value.
+   *
+   * @param a first value.
+   * @param b second value.
+   * @return result of Multiplication.
+   */
   public int mul(int a, int b) {
     return a * b;
   }
 
+  /**
+   * Function Divide two value.
+   *
+   * @param a first value.
+   * @param b second value.
+   * @return result of Division.
+   */
   public int div(int a, int b) {
     return a / b;
   }
 
+  /**
+   * Check if Value is Pair.
+   *
+   * @param a value to check.
+   * @return True if pair.
+   */
   public boolean isPair(int a) {
     return (a % 2) == 0;
   }
