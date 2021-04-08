96799a795d76a16f333b932101e2d7bdc9f2cc83
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2021-04-08 10:01:50 +0200

    Add calculator to main

--- src/main/java/com/m2i/devops/Application.java
+++ src/main/java/com/m2i/devops/Application.java
@@ -4,6 +4,9 @@ public class Application {
 
   public static void main(String[] args) {
     System.out.println("Hello W !");
+
+    Calculator calc = new Calculator();
+    int result = calc.add(2, 3);
   }
 
 }
